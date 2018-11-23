24772051c746cdfa9cc244718d1007646f110af3
Andres Armua <andres.armua@ed.ac.uk>
2018-11-23 14:31:38 +0000

    modified:   Planet.h

--- Planet.h
+++ Planet.h
@@ -11,15 +11,21 @@ class Planet {
 	public:
 	
 	Planet();
-	Planet(string name, double mass, double x);
+	Planet(string name, double mass, double x, double y, double vy, double vx);
     void set_name(string name);
     void set_mass(double mass);
 	void set_x(double x);
+	void set_y(double x);
+	void set_vx(double x);
+	void set_vy(double x);
 
       
     string get_name();
     double get_mass();
     double get_x(); 
+    double get_y(); 
+    double get_vx(); 
+    double get_vy(); 
    
    
    
@@ -30,6 +36,9 @@ class Planet {
 	string m_name;
 	double m_mass;
     double m_x;
+    double m_y;
+    double m_vx;
+    double m_vy;
 
 
 };  // is this ';' necessary?
